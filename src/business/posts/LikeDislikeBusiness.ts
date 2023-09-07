a2c4e753130f03304705eed5b8303044899ec485
bartomsilva <bartomsilva@gmail.com>
2023-09-07 17:27:39 -0300

    feat: likes e dislikes

--- src/business/posts/LikeDislikeBusiness.ts
+++ src/business/posts/LikeDislikeBusiness.ts
@@ -2,7 +2,7 @@ import { LikesDislikesDatabase } from "../../database/posts/LikeDislikeDatabase"
 import { LikeDislikeInputDTO } from "../../dtos/posts/likeDislike.dto"
 import { BadRequestError } from "../../error/BadRequest"
 import { NotFoundError } from "../../error/NotFound"
-import { LikesDislikesDB, PostDB } from "../../models/posts/Post"
+import { ActionDB, LikesDislikesDB, POST_ACTION } from "../../models/posts/Post"
 import { TokenManager } from "../../services/TokenManager"
 
 export class LikeDislikeBusiness {
@@ -14,8 +14,8 @@ export class LikeDislikeBusiness {
   // tratativas da ação: like / dislike    
   public likeDislike = async (input: LikeDislikeInputDTO): Promise<void> => {
 
-    // pega o id do post 
-    const { id: postId, like, token } = input
+    // pega o id do post / comment
+    const { id: actionId, like, action, token } = input
     const likeVal: number = like ? 1 : 0
 
     // validação token 
@@ -30,49 +30,55 @@ export class LikeDislikeBusiness {
     // monta o objeto de likes dislikes
     const postLikeDislike: LikesDislikesDB = {
       user_id: userId,
-      post_id: postId,
+      action_id: actionId,
       like: likeVal
     }
 
-    // procurar o post 
-    const [post]: PostDB[] = await this.likesDislikesDataBase.findPost(postId)
-    if (post === undefined) {
-      throw new NotFoundError("post não encontrado")
+    // procurar o post / comment
+    let postComment: ActionDB
+    if ( action == POST_ACTION.POST){
+      [postComment] = await this.likesDislikesDataBase.findPost(actionId)
+    } else {
+      [postComment] = await this.likesDislikesDataBase.findComment(actionId)
     }
 
-    // Verifica se o post é do mesmo usuário
-    if (post.creator_id === userId) {
+    if (postComment === undefined) {
+      throw new NotFoundError("id não encontrado")
+    }
+
+    // Verifica se o post/comment é do mesmo usuário
+    if (postComment.creator_id === userId) {
       throw new BadRequestError("ação inválida")
     }
 
     // pesquisa se ( likes_dislikes ) se existe registro: User x Post
     const likeDislikeDB: LikesDislikesDB = 
-    await this.likesDislikesDataBase.findLikeDislike(postId, userId)
+    await this.likesDislikesDataBase.findLikeDislike(actionId, userId)
 
     // inserir na tabela (caso não haja registro)
     if (likeDislikeDB === undefined) {
       await this.likesDislikesDataBase.insertLikeDislike(postLikeDislike)
       if (likeVal === 1) {
-        await this.likesDislikesDataBase.postIncreaseLike(postId)
+        await this.likesDislikesDataBase.postIncreaseLike(actionId)
       } else {
-        await this.likesDislikesDataBase.postIncreaseDislike(postId)
+        await this.likesDislikesDataBase.postIncreaseDislike(actionId)
       }
     } else { // existe um registro entre User x Post 
       // deletar o like/Dislike se for a mesma opção
       if (likeVal == likeDislikeDB.like) {
-        await this.likesDislikesDataBase.deleteLikeDislike(postId, userId)
+        await this.likesDislikesDataBase.deleteLikeDislike(actionId, userId)
         if (likeVal === 1) {
-          await this.likesDislikesDataBase.postDecreaseLike(postId)
+          await this.likesDislikesDataBase.postDecreaseLike(actionId)
         } else {
-          await this.likesDislikesDataBase.postDecreaseDislike(postId)
+          await this.likesDislikesDataBase.postDecreaseDislike(actionId)
         }
       } else { // trocar de like para dislike ou dislike para like (inverter)
         await this.likesDislikesDataBase.updateLikeDislike(postLikeDislike)
         if (likeVal==1){
-          await this.likesDislikesDataBase.postReverseDislikeToLike(postId)
+          await this.likesDislikesDataBase.postReverseDislikeToLike(actionId)
         }
         else {
-          await this.likesDislikesDataBase.postReverseLikeToDislike(postId)
+          await this.likesDislikesDataBase.postReverseLikeToDislike(actionId)
         }
       }
     }
