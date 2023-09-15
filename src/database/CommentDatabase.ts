630d6d81420f754d798f14dc52293718c9ce6233
bartomsilva <bartomsilva@gmail.com>
2023-09-15 15:06:23 -0300

    exclusão na tabela de likes e dislikes

--- src/database/CommentDatabase.ts
+++ src/database/CommentDatabase.ts
@@ -22,29 +22,31 @@ export class CommentDataBase extends BaseDataBase {
   public deleteComment = async (commentId: string): Promise<void> => {
     await BaseDataBase.connection(this.TABLE_NAME)
       .del().where("id", "=", commentId)
+    await BaseDataBase.connection("likes_dislikes")
+      .del().where({ action_id: commentId })
   }
 
   //============ GET COMMENTS FOR POST
-  public getComment = async (postId: string):Promise<CommentResultDB[]> => {
+  public getComment = async (postId: string): Promise<CommentResultDB[]> => {
 
-    const output:CommentResultDB[] =  await BaseDataBase.connection("comments as p")
-      .select("p.id","p.post_id", "p.content", "p.likes", "p.dislikes",
-      "p.comments","p.creator_id", "u.name as creator_name")        
+    const output: CommentResultDB[] = await BaseDataBase.connection("comments as p")
+      .select("p.id", "p.post_id", "p.content", "p.likes", "p.dislikes",
+        "p.comments", "p.creator_id", "u.name as creator_name")
       .innerJoin("users as u", "p.creator_id", "u.id")
-      .where({post_id: postId})
-      return output
+      .where({ post_id: postId })
+    return output
   }
 
   // rotitna de que incrementa o número de comentarios dos posts
-  public incrementComments = async (postId: string):Promise<void> =>{
+  public incrementComments = async (postId: string): Promise<void> => {
     await BaseDataBase.connection("posts")
-    .where({ id: postId })
-    .increment("comments")
+      .where({ id: postId })
+      .increment("comments")
   }
   // rotitna de que decrementa o número de comentarios dos posts
-  public decrementComments = async (postId: string):Promise<void> =>{
+  public decrementComments = async (postId: string): Promise<void> => {
     await BaseDataBase.connection("posts")
-    .where({ id: postId })
-    .decrement("comments")
+      .where({ id: postId })
+      .decrement("comments")
   }
 }
