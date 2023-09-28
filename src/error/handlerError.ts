da39b00ea8f36c978c6eca55fc02f7f2d84ce54a
bartomsilva <bartomsilva@gmail.com>
2023-09-27 22:15:51 -0300

    deply teste

--- src/error/handlerError.ts
+++ src/error/handlerError.ts
@@ -5,6 +5,7 @@ import { ZodError } from "zod"
 import { HTTP_CODE } from "../util/util"
 
 export const handlerError = (res: Response, error: unknown) => {
+  console.log(error)
   if (error instanceof ZodError) {
     res.status(HTTP_CODE.BAD_REQUEST).send(error.issues[0])
   } else if (error instanceof BaseError) {
